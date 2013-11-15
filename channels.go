a50c19a106ffd2e777d77eea64f8bb66f922fca3
Evan Huus <eapache@gmail.com>
2013-11-15 17:44:18 -0500

    move Pipe into channels.go and remove utils.go

--- channels.go
+++ channels.go
@@ -29,3 +29,14 @@ type Channel interface {
 	InChannel
 	Out() <-chan interface{}
 }
+
+// Pipe connects the input channel to the output channel so that
+// they behave as if a single channel.
+func Pipe(input OutChannel, output InChannel) {
+	go func() {
+		for elem := range input.Out() {
+			output.In() <- elem
+		}
+		output.Close()
+	}()
+}
