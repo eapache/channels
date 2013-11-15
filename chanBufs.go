6b9405f5a4b2079e20524ce89856250cbe6aa2f2
Evan Huus <eapache@gmail.com>
2013-11-14 19:28:22 -0500

    Initial infinite buffer

---
+++ chanBufs.go
@@ -0,0 +1,42 @@
+package chanBufs
+
+// NewInfiniteBuffer produces a read-only channel and a write-only channel that
+// behave exactly like the two ends of a single go channel with an infinite buffer.
+// Be very careful using this, as no buffer is truly infinite - if the internal
+// buffer grows too large your program will run out of memory and crash.
+func NewInfiniteBuffer() (chan<- interface{}, <-chan interface{}) {
+	input := make(chan interface{})
+	output := make(chan interface{})
+
+	go func() {
+		var buffer []interface{}
+		for {
+			if len(buffer) == 0 {
+				elem, open := <-input
+				if open {
+					buffer = append(buffer, elem)
+				} else {
+					close(output)
+					return
+				}
+			} else {
+				select {
+				case elem, open := <-input:
+					if open {
+						buffer = append(buffer, elem)
+					} else {
+						for elem := range buffer {
+							output <- elem
+						}
+						close(output)
+						return
+					}
+				case output <- buffer[0]:
+					buffer = buffer[1:]
+				}
+			}
+		}
+	}()
+
+	return input, output
+}
