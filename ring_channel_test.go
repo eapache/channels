3444811fd18b006d9690e9de0b3a8534977e7b56
Evan Huus <eapache@gmail.com>
2014-06-06 23:36:26 -0400

    Add more corner tests

--- ring_channel_test.go
+++ ring_channel_test.go
@@ -8,6 +8,20 @@ func TestRingChannel(t *testing.T) {
 	ch = NewRingChannel(Infinity) // yes this is rather silly, but it should work
 	testChannel(t, "infinite ring-buffer channel", ch)
 
+	ch = NewRingChannel(None)
+	go func() {
+		for i := 0; i < 1000; i++ {
+			ch.In() <- i
+		}
+		ch.Close()
+	}()
+	prev := -1
+	for i := range ch.Out() {
+		if prev >= i.(int) {
+			t.Fatal("ring channel prev", prev, "but got", i.(int))
+		}
+	}
+
 	ch = NewRingChannel(10)
 	for i := 0; i < 1000; i++ {
 		ch.In() <- i
